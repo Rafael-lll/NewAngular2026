2d5a92de9e71c5c3730e8d0f373aa39637c556d7
Rafael-lll <rafael11052006@gmail.com>
2026-09-03 21:29:53 -0300

    Injeção de dependências

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,6 +1,8 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { Produto } from '../../../model/produto';
 import { CardProduto } from "../card-produto/card-produto";
+import { ProdutoService } from '../services/produto.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -9,53 +11,20 @@ import { CardProduto } from "../card-produto/card-produto";
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
+
+  private produtoService = inject(ProdutoService);
+
+  private produtos = toSignal<Produto[], Produto[]>( this.produtoService.listar(), {initialValue: []});
   apenasPromo = signal(false);
 
   produtosExibidos = computed(() =>
-   this.apenasPromo() ? this.produtos.filter(p => p.promo): this.produtos);
+   this.apenasPromo() ? this.produtos().filter(p => p.promo): this.produtos());
 
   alterarPromo(){
     this.apenasPromo.update(v => !v)
   }
 
-  produtos = <Produto[]>[
-    {
-    id: 1,
-    nome: 'Parafal',
-    preco: 39000.00,
-    descricao: 'Sai mais barato do que perder a vida',
-    imageUrl: 'images/parafal.jpg',
-    promo: false,
-    estado: 'usado'
-  },
-    {
-    id: 2,
-    nome: 'hk',
-    preco: 17000.00,
-    descricao: 'metralhadora alemã ou de israel...',
-    imageUrl: 'images/hk.jpg',
-    promo: false,
-    estado: 'novo'
-  },
-    {
-    id: 3,
-    nome: 'Beretta',
-    preco: 8200.00,
-    descricao: 'Otima para auto defesa.',
-    imageUrl: 'images/beretta.jpg',
-    promo: true,
-    estado: 'esgotado'
-  },
-  {
-    id: 4,
-    nome: 'AK-47',
-    preco: 9500.00,
-    descricao: 'fds.',
-    imageUrl: 'images/ak.jpg',
-    promo: false,
-    estado: 'novo'
-  }
-];
+  
 
 onViewProduct(id: number){
   alert('Visualizando produto id: ' +id);
